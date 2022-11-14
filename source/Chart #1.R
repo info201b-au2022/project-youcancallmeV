0a58b04df5fce96eef84e1328fa973a143441ac4
risan03-2165658 <risan03@uw.edu>
2022-11-14 13:26:31 -0800

    aa

--- source/Chart #1.R
+++ source/Chart #1.R
@@ -23,12 +23,14 @@ college_admission_aggregated <- college_admission %>%
     Total.price.for.in.state.students.living.on.campus.2013.14,
     Total.price.for.out.of.state.students.living.on.campus.2013.14
   )
-
+# Since we are calculating average, it would be very inconvenient if the school's data is not available
+# Thus, we will be excluding the schools that contains NA values in the columns we desire 
 college_admission_no_NA <- college_admission_aggregated[complete.cases(college_admission_aggregated), ]
 
+#Creates a graph that presents the State versus the total price for our of state students living on campus (2013)
 ggplot(data = college_admission_no_NA) +
   geom_point(
-    mapping = aes(x = Name , y = Total.price.for.out.of.state.students.living.on.campus.2013.14),
+    mapping = aes(x = State.abbreviation , y = Total.price.for.out.of.state.students.living.on.campus.2013.14),
     color = "red",
     alpha = .3
   )
